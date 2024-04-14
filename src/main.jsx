217bc9b6e0f2eb99437d60608c49f000978ed258
minjeongHEO <hmjoung9770@gmail.com>
2024-04-14 23:40:26 +0900

    feat: chapter5 파일 설정

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import AppProfile from "./AppProfile.jsx";
-import AppCounter from "./AppCounter.jsx";
-import AppProducts from "./AppProducts.jsx";
+import AppXY from "./AppXY.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <AppProducts />
+        <AppXY />
     </React.StrictMode>
 );
