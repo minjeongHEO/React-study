77fa7583af5343060a10a1a69bd41f03b27645d7
minjeongHEO <hmjoung9770@gmail.com>
2024-04-15 13:17:55 +0900

    feat: 5.3 마우스 따라 가기 - 기본 구현💡

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./AppXY.css";
 
 export default function AppXY() {
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
+    const movePointerXY = (e) => {
+        console.log(e);
+        setX(e.clientX);
+        setY(e.clientY);
+    };
     return (
-        <div className='container'>
-            <div className='pointer'></div>
+        <div className="container" onPointerMove={movePointerXY}>
+            <div
+                className="pointer"
+                style={{ transform: `translate(${x}px, ${y}px)` }}
+            ></div>
         </div>
     );
 }
