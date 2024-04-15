5dfaaa28a5129e061ca4dbd220ef888bbc21331a
minjeongHEO <hmjoung9770@gmail.com>
2024-04-15 13:50:26 +0900

    feat: 5.6 중첩 객체 상태 관리 (멘토 바꾸기)💡

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import AppXY from "./AppXY.jsx";
+import AppMentor from "./AppMentor.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <AppXY />
+        <AppMentor />
     </React.StrictMode>
 );
