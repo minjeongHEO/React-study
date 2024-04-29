637161f0fc9b152a1c8d994e55e36fa6e844978f
minjeongHEO <hmjoung9770@gmail.com>
2024-04-29 12:00:24 +0900

    feat: 5.14 Form을 만드는 법

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,10 @@ import AppXY from "./AppXY.jsx";
 import AppMentor from "./AppMentor.jsx";
 import AppMentors from "./AppMentors.jsx";
 import AppMentorsImmer from "./AppMentorsImmer.jsx";
+import AppForm from "./AppForm.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <AppMentorsImmer />
+        <AppForm />
     </React.StrictMode>
 );
