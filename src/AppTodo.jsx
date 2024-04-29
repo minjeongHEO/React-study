b69164162caca2f5eaf131ce979bd1f7346a1b51
minjeongHEO <hmjoung9770@gmail.com>
2024-04-29 22:35:19 +0900

    feat: 리스트 추가하기

--- src/AppTodo.jsx
+++ src/AppTodo.jsx
@@ -1,25 +1,40 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { CgDarkMode } from "react-icons/cg";
+import { FaTrashAlt } from "react-icons/fa";
 import "./AppTodo.css";
 
 export default function AppTodo() {
     const [todoItem, setTodoItem] = useState({ active: [], completed: [] });
     const [filterType, setFilterType] = useState("All");
-    const [form, setForm] = useState("");
+    const [form, setForm] = useState({ id: "", value: "" });
+    const [input, setInput] = useState("");
 
     const showByFilter = e => {
         const targetText = e.target.textContent;
         setFilterType(targetText);
     };
 
-    const handleSubmit = e => {
-        e.preventDefault();
-        setTodoItem(prev => ({ ...prev, active: [...prev.active, form] }));
+    const handleChange = ({ target }) => {
+        const { value } = target;
+        setInput(value);
     };
 
-    function handleChange({ target }) {
-        setForm(target.value);
-    }
+    // 랜덤id생성
+    const randomId = () => {
+        return new Date().getTime().toString();
+    };
+
+    // Add
+    const addTodoList = () => {
+        const id = randomId();
+        setTodoItem(prev => ({ ...prev, active: [...prev.active, { id, value: input }] }));
+    };
+
+    // 체크박스
+    const changeCheckBox = () => {
+        // setTodoItem(prev => ({ ...prev, active: [...prev.active, form] }));
+    };
+
     return (
         <div className='container'>
             <nav>
@@ -30,14 +45,17 @@ export default function AppTodo() {
             </nav>
             <main>
                 <ul>
-                    {todoItem.active.map((item, idx) => (
-                        <li key={idx}>{item}</li>
+                    {todoItem.active.map(todoItem => (
+                        <li key={todoItem.id}>
+                            <input type='checkbox' onChange={changeCheckBox} />
+                            {todoItem.value}
+                            <FaTrashAlt />
+                        </li>
                     ))}
                 </ul>
-                <form onSubmit={handleSubmit}>
-                    <input type='text' id='text' name='text' onChange={handleChange} value={form}></input>
-                    <button>Add</button>
-                </form>
+
+                <input type='text' name='text' onChange={handleChange} value={input}></input>
+                <button onClick={addTodoList}>Add</button>
             </main>
         </div>
     );
