e2010e60464a44c28ffaca5c63eddf32cafa0425
minjeongHEO <hmjoung9770@gmail.com>
2024-04-29 17:53:27 +0900

    feat: 아이템 추가

--- src/AppTodo.jsx
+++ src/AppTodo.jsx
@@ -3,24 +3,41 @@ import { CgDarkMode } from "react-icons/cg";
 import "./AppTodo.css";
 
 export default function AppTodo() {
+    const [todoItem, setTodoItem] = useState({ active: [], completed: [] });
+    const [filterType, setFilterType] = useState("All");
+    const [form, setForm] = useState("");
+
+    const showByFilter = e => {
+        const targetText = e.target.textContent;
+        setFilterType(targetText);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        setTodoItem(prev => ({ ...prev, active: [...prev.active, form] }));
+    };
+
+    function handleChange({ target }) {
+        setForm(target.value);
+    }
     return (
         <div className='container'>
             <nav>
                 <CgDarkMode />
-                <span>All</span>
-                <span>Active</span>
-                <span>Completed</span>
+                <span onClick={showByFilter}>All</span>
+                <span onClick={showByFilter}>Active</span>
+                <span onClick={showByFilter}>Completed</span>
             </nav>
             <main>
                 <ul>
-                    <li>할일 목록들</li>
-                    <li>할일 목록들</li>
-                    <li>할일 목록들</li>
+                    {todoItem.active.map((item, idx) => (
+                        <li key={idx}>{item}</li>
+                    ))}
                 </ul>
-                <div>
-                    <input type='email' id='email' name='email'></input>
+                <form onSubmit={handleSubmit}>
+                    <input type='text' id='text' name='text' onChange={handleChange} value={form}></input>
                     <button>Add</button>
-                </div>
+                </form>
             </main>
         </div>
     );
