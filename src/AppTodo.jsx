f80d125323eb2c038f84c3ab3b9f3bcb33f0821e
minjeongHEO <hmjoung9770@gmail.com>
2024-04-29 16:20:07 +0900

    feat: react-icons 적용하기

--- src/AppTodo.jsx
+++ src/AppTodo.jsx
@@ -1,6 +1,27 @@
 import React, { useState } from "react";
-import "./AppXY.css";
+import { CgDarkMode } from "react-icons/cg";
+import "./AppTodo.css";
 
 export default function AppTodo() {
-    return <div>ddd</div>;
+    return (
+        <div className='container'>
+            <nav>
+                <CgDarkMode />
+                <span>All</span>
+                <span>Active</span>
+                <span>Completed</span>
+            </nav>
+            <main>
+                <ul>
+                    <li>할일 목록들</li>
+                    <li>할일 목록들</li>
+                    <li>할일 목록들</li>
+                </ul>
+                <div>
+                    <input type='email' id='email' name='email'></input>
+                    <button>Add</button>
+                </div>
+            </main>
+        </div>
+    );
 }
